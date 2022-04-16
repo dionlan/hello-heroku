74b8014579eeb86194196ca136f1c1013686ef30
dionlan <dionlan.alves@gmail.com>
2022-04-16 12:38:54 -0300

    Com alteração.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World Build to Heroku.
+          Hello World Build to Heroku com alteração.
         </p>
         <a
           className="App-link"
